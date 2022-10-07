cfdad7e2914e7e3a35b19010968b2b9866d11214
Kai Takami <kaitakami@protonmail.com>
2022-10-07 20:55:20 +0900

    Set up first configuration with tailwind and webpack installation

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Plugin
+const path = require("path");
+
+const ruleForStyles = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader", "postcss-loader"],
+};
+
+const ruleForHtml = {
+  test: /\.html$/i,
+  loader: "html-loader",
+};
+
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProduction = mode === "production";
+  return {
+    // entry: './src/index.js',
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "main.js", // Change the filename
+      path: path.resolve(__dirname, "build"), // Change the folder name
+    },
+    module: {
+      rules: [ruleForStyles, ruleForHtml],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "src/index.html" }), // Modify template if necessary
+    ],
+    devServer: {
+      open: true,
+    },
+  };
+};
