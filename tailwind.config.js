cd87b0e98feaa53527108f66d86e6a8c6cf1ac4c
Kai Takami <kaitakami@protonmail.com>
2022-10-08 20:48:18 +0900

    layer components of tailwindcss and font family Exo 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "rgba(42, 42, 56, 0.8)",
+        "gray-dark": "#3b3b48",
+        gray: "#595976",
+        "gray-light": "rgba(209, 209, 223, 0.7)",
+        "white": "#d1d1df",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
