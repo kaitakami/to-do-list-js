b36fa2aa84834f78e404dcecdf0de8e2340f9409
Kai Takami <kaitakami@protonmail.com>
2022-10-10 21:10:45 +0900

    Create delete function and change status

--- src/modules/addTaskLogic.js
+++ src/modules/addTaskLogic.js
@@ -1,17 +1,42 @@
-const tasks = [
+import { tasksSetLS, tasksLocalStorage } from "./localStorage";
+
+let tasks = tasksLocalStorage || [
   {
     name: "Start by adding a new Task!",
     date: new Date().toLocaleDateString(),
+    checked: false,
     group: {
       name: undefined,
       color: undefined,
     },
-    id: 1,
+    id: "1",
   },
 ];
 
 const getUserData = (newTask) => {
   tasks.push(newTask);
+  tasksSetLS(tasks);
 };
 
-export { getUserData, tasks };
+const deleteTask = (taskId) => {
+  const tempTasks = tasks.filter((task) => task.id !== taskId);
+  tasks = [...tempTasks];
+  tasksSetLS(tasks);
+};
+
+const changeCheckedStatus = (taskId) => {
+  const tempTasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      {
+        return { ...task, checked: !task.checked };
+      }
+    } else {
+      return task;
+    }
+  });
+  tasks = [...tempTasks];
+  console.log(tasks)
+  tasksSetLS(tasks);
+};
+
+export { tasks, getUserData, deleteTask, changeCheckedStatus };
