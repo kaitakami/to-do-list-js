22d9ed08f0abefd0dd8e31aff7e47586173f3d22
Kai Takami <kaitakami@protonmail.com>
2022-10-21 22:08:15 +0900

    Add local storage for groups and selected group

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -1,7 +1,27 @@
 let tasksLocalStorage = JSON.parse(localStorage.getItem("tasks"));
 
+let groupsLocalStorage = JSON.parse(localStorage.getItem("groups"));
+
+let selectedGroupLocalStorage = JSON.parse(localStorage.getItem("selectedGroup"))
+
 const tasksSetLS = (tasksArr) => {
   localStorage.setItem("tasks", JSON.stringify(tasksArr));
 };
 
-export { tasksSetLS, tasksLocalStorage };
+const groupsSetLS = (groupsArr) => {
+  localStorage.setItem("groups", JSON.stringify(groupsArr));
+};
+
+const selectedGroupSetLS = (selectedGroup) => {
+  localStorage.setItem("selectedGroup", JSON.stringify(selectedGroup));
+};
+
+
+export {
+  tasksSetLS,
+  tasksLocalStorage,
+  groupsSetLS,
+  groupsLocalStorage,
+  selectedGroupLocalStorage,
+  selectedGroupSetLS,
+};
